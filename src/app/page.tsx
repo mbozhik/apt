c6db1ab9a5c69f03d814e1f4afb02e342e799329
maxim <bozzhik@ya.ru>
2025-05-14 23:46:35 +0300

    init `Hero` index block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+'use client'
+
+import {PROJECT_CONTAINER} from '@/lib/constants'
+import {cn} from '@/lib/utils'
+
+import Hero from '~~/index/Hero'
+
 export default function Home() {
-  return <div className="p-6 animate-bounce">apt catalog</div>
+  return (
+    <main className={cn(PROJECT_CONTAINER, 'relative grid grid-cols-2 sm:grid-cols-1 sm:gap-12')}>
+      <Hero />
+
+      <div className="w-full sm:w-20 h-[50vh] col-start-2 sm:col-start-auto bg-orange"></div>
+    </main>
+  )
 }

8d3d8ad0ed7e44addbac4a8b76602cc46ad32889
maxim <bozzhik@ya.ru>
2025-05-15 16:45:49 +0300

    integrate `getTires` with `sanity` data in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+import {getTires} from '@/sanity/lib/requests'
 
 import {PROJECT_CONTAINER} from '@/lib/constants'
 import {cn} from '@/lib/utils'
@@ -6,12 +6,14 @@ import {cn} from '@/lib/utils'
 import Hero from '~~/index/Hero'
 import Catalog from '~~/index/Catalog'
 
-export default function Home() {
+export default async function IndexPage() {
+  const tires = await getTires()
+
   return (
     <main className={cn(PROJECT_CONTAINER, 'relative grid grid-cols-2 sm:grid-cols-1 sm:gap-6')}>
       <Hero />
 
-      <Catalog className="col-start-2 sm:col-start-auto" />
+      <Catalog className="col-start-2 sm:col-start-auto" items={tires} />
     </main>
   )
 }
--- src/components/App/index/Catalog.tsx
+++ src/components/App/index/Catalog.tsx
@@ -1,34 +1,32 @@
-import MockImage from '$/logo.svg'
+'use client'
+
+import type {TIRE_QUERYResult} from '@/sanity/lib/requests'
 
 import {cn} from '@/lib/utils'
 import {motion} from 'motion/react'
 
+import Link from 'next/link'
 import Image from 'next/image'
+import {urlFor} from '@/sanity/lib/image'
 
-function Card() {
+function Card({data}: {data: TIRE_QUERYResult[number]}) {
   return (
     <div data-block="card-catalog-index" className={cn('p-10 xl:p-6 sm:p-3.5 sm:pb-6 flex flex-col gap-8 sm:gap-6', 'bg-white text-black')}>
-      <div className="flex sm:flex-col sm:gap-4 justify-between">
+      <div className="flex justify-between sm:flex-col sm:gap-4">
         <div className="space-y-8 sm:space-y-3">
-          <h3 className="text-4xl sm:text-3xl font-bold text-orange">
-            APT TIRES NON MARKING
-            <br />
-            WITH APERTURE
-          </h3>
+          <h3 className="text-4xl font-bold sm:text-3xl text-orange">{data.naming}</h3>
 
-          <div className={cn('w-fit flex items-center gap-2', 'text-2xl xl:text-xl', 'pb-0.5 border-b-2 border-b-black', 'group')}>
+          <Link href={`/tire/${data.slug?.current}`} className={cn('w-fit flex items-center gap-2', 'text-2xl xl:text-xl', 'pb-0.5 border-b-2 border-b-black', 'group')}>
             Характеристики
-            <span className="group-hover:translate-x-2 duration-200">→</span>
-          </div>
+            <span className="duration-300 group-hover:translate-x-1">→</span>
+          </Link>
         </div>
 
-        <div className="size-48 sm:size-full xl:aspect-square relative bg-black">
-          <Image className="object-contain" src={MockImage} alt="Catalog Item" fill />
-        </div>
+        <div className="relative size-48 sm:size-full xl:aspect-square">{data.image && <Image className="object-contain" src={urlFor(data.image).url()} alt={data.naming} fill />} </div>
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-1 gap-6 sm:gap-4">
-        {['Прогрессивная конструкция гарантирует эффективную работу даже в самых экстремальных условиях в режиме 24/7', 'Особая прочность позволяет шине оставаться без повреждений в тех случаях, когда обычная пневматическая покрышка требует срочного ремонта', 'Специальная строение шины дает повышенную устойчивость и идеальное распределение веса при работе, а как следствие, наибольшую безопасность для оператора при рузных и других работах', 'Увеличенная глубина протектора обеспечивает ресурс в 5-6 раза выше, чем у пневматической шины'].map((item, index) => (
+      <div className="grid grid-cols-2 gap-6 sm:grid-cols-1 sm:gap-4">
+        {data.descriptors?.map((item, index) => (
           <div key={index} className="relative flex gap-4 sm:gap-3">
             <div className="min-w-0.5 bg-orange h-full" />
 
@@ -40,19 +38,19 @@ function Card() {
   )
 }
 
-export default function Catalog({className}: {className?: string}) {
+export default function Catalog({items, className}: {items: TIRE_QUERYResult; className?: string}) {
   return (
     <section data-section="catalog-index" className={cn(className)}>
       <div className={cn('px-6 xl:px-0', 'space-y-8 xl:space-y-6')}>
-        {[1, 2, 3, 4].map((item, index) => (
+        {items.map((item, idx) => (
           <motion.div
-            key={item}
-            initial={{opacity: 0, y: index === 0 ? 0 : 50}} // first not moving
+            key={idx}
+            initial={{opacity: 0, y: idx === 0 ? 0 : 50}} // first not moving
             whileInView={{opacity: 1, y: 0}}
-            transition={{duration: 0.5, delay: index * 0.1}}
+            transition={{duration: 0.5, delay: idx * 0.1}}
             viewport={{once: true}}
           >
-            <Card />
+            <Card data={item} />
           </motion.div>
         ))}
       </div>

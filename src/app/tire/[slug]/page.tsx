6498a8665a7f197f396be52e3addd889df2c042e
maxim <bozzhik@gmail.com>
2025-08-08 02:20:24 +0700

    init `SheetSkeleton` tire block

--- src/app/tire/[slug]/page.tsx
+++ src/app/tire/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next'
+import {Suspense} from 'react'
 
 import {PROJECT_CONTAINER} from '@/lib/constants'
 import {getTireItem} from '@/sanity/lib/requests'
@@ -6,6 +7,7 @@ import {getTireItem} from '@/sanity/lib/requests'
 import {cn} from '@/lib/utils'
 import Details from '~~/tire/Details'
 import Sheet from '~~/tire/Sheet'
+import SheetSkeleton from '~~/tire/SheetSkeleton'
 
 type Props = {
   params: Promise<{slug: string}>
@@ -28,7 +30,9 @@ export default async function TireItemPage({params}: Props) {
     <main className={cn(PROJECT_CONTAINER, 'space-y-20 sm:space-y-14')}>
       <Details data={tire} />
 
-      <Sheet token={tire?.token.current as string} />
+      <Suspense fallback={<SheetSkeleton />}>
+        <Sheet token={tire?.token.current as string} />
+      </Suspense>
     </main>
   )
 }
